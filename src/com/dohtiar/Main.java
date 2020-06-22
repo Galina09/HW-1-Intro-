428507b3ff9f61556f48afac48efe2d295ce0aaf
Halyna Dohtiar <galinavia9@gmail.com>
2020-06-23 02:25:29 +0300

    first

---
+++ src/com/dohtiar/Main.java
@@ -0,0 +1,19 @@
+package com.dohtiar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 125;
+        short b = 5;
+        int c =1000;
+        long d = 500;
+        float e = 200.1f;
+        double f = 400;
+
+        System.out.println(a+b);
+        System.out.println(c-d);
+        System.out.println(e/f);
+        System.out.println(a*b);
+
+    }
+}
